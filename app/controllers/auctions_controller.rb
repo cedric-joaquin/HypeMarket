727c49cd89552f9f1ae92ebfeefdfef7095c9ad2
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-09 16:24:18 -0700

    added actions to auctions controller

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,2 +1,19 @@
 class AuctionsController < ApplicationController
+    before_action :require_login
+    skip_before_action :require_login, only: [:index, :show]
+
+    def index
+        @auctions = Auction.all
+    end
+
+    def show
+        @auction = Auction.find_by(id: params[:id])
+    end
+
+    def new
+    end
+
+    def create
+
+    end
 end
