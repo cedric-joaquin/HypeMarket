f70115518f957df4862f6af7ea36858f7b07260c
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-09 10:02:13 -0700

    add comment for reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_many :auctions, foreign_key: :seller_id, class_name: "Auction"
     has_many :bids, foreign_key: :bidder_id, class_name: "Bid"
 
-    #looks for association :auction in the Bid model
+    #looks for association :auction in the Bid model to determine auctions
+    #this user bid on
     has_many :bidded_auctions, through: :bids, source: :auction
 end
