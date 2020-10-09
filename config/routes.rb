19c5769faf818e16476e85ffd439852dbe1f2b45
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-09 15:04:12 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :bids
   resources :auctions
-  resources :users
+  resources :users, only: [:show, :create]
+
+  get '/signup' => 'users#new'
+
+  get '/login' => "sessions#new"
+  post '/login' => "sessions#create"
+  post '/logout' => "sessions#destroy"
+
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
