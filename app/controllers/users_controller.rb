96f5d37193f361c2718444596406a502099beaf1
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-09 16:52:40 -0700

    set session once signed up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     #signup
     def create
         user = User.create(user_params)
+        session[:user_id] = user.id
         redirect_to user
     end
 
