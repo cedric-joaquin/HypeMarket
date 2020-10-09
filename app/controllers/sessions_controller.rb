fce3cd2148a0d406508b4777747a7f7904a84023
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-09 14:56:43 -0700

    added login and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:user][:username])
+    return head(:forbidden) unless user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect_to user_path(user)
   end
 
   def destroy
