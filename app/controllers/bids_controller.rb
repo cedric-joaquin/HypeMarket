115d7a8f910d93c68cf6b7f0a4be1b616e6a18b2
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-10 12:00:53 -0700

    added bid create action

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -2,7 +2,8 @@ class BidsController < ApplicationController
     before_action :require_login
     
     def create
-        
+        bid = Bid.create(bid: params[:bid][:bid], bidder: current_user, auction: Auction.find_by(id: params[:bid][:auction][:id]))
+        redirect_to auction_path(bid.auction)
     end
 
     def edit
