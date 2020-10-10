80234e51933615346a63949d2154445ed653e49a
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-10 11:56:18 -0700

    added auction id hidden_field

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,2 +1,16 @@
 class BidsController < ApplicationController
+    before_action :require_login
+    
+    def create
+        
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+    def destroy
+    end
 end
