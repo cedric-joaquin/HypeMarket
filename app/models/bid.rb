0e1a68e2155392bedac4bdfb9a5b61b37e558fed
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-10 18:05:30 -0700

    updated bid history scope method

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,5 +2,5 @@ class Bid < ApplicationRecord
     belongs_to :bidder, foreign_key: :bidder_id, class_name: "User"
     belongs_to :auction
 
-    scope :bid_history, -> { (order("bid asc")) }
+    scope :bid_history, -> { (order("bid desc")) }
 end
