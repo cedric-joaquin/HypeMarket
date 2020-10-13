e88ada64af0ae61b4c54198df8a96aef703a6ec5
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-12 20:36:08 -0700

    add form validations for signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,19 @@ class UsersController < ApplicationController
     #signup form
     def new
         logged_in_redirect
+        @user = User.new
     end
 
     #signup
     def create
-        user = User.create(user_params)
-        session[:user_id] = user.id
-        redirect_to user
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            session[:user_id] = user.id
+            redirect_to user
+        else
+            render :new
+        end
     end
 
     def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
     has_many :auctions, foreign_key: :seller_id, class_name: "Auction"
     has_many :bids, foreign_key: :bidder_id, class_name: "Bid"
 
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+
+
     #looks for association :auction in the Bid model to determine auctions
     #this user bid on
     has_many :bidded_auctions, through: :bids, source: :auction
