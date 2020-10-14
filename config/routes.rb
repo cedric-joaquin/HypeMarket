7c5b84b60830fe118bce2772a7b27ead6fe89d15
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-13 17:39:40 -0700

    add oauth route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   post '/login' => "sessions#create"
   post '/logout' => "sessions#destroy"
 
+  get '/auth/:provider/callback' => 'sessions#create'
+
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
