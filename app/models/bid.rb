8ebdfa6d25042d90b99f704e1b2f56cf2f44e7b0
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-24 11:45:25 -0700

    bug fixes

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,8 +2,7 @@ class Bid < ApplicationRecord
     belongs_to :bidder, foreign_key: :bidder_id, class_name: "User"
     belongs_to :auction
 
-    validates :bid, presence: true
-    validates :bid, numericality: { greater_than: :bid_was}, on: :update
+    validates :bid, presence: true, on: :create
 
     scope :bid_history, -> { (order("bid desc")) }
     scope :highest_bid, -> { bid_history.limit(1).first }
