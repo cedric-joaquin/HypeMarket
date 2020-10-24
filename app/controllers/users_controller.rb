3cdf4c5ad723dbcb20c002de0fd174654945c296
cedric-joaquin <joaquin.cedric@gmail.com>
2020-10-24 11:08:00 -0700

    bug fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.valid?
             @user.save
-            session[:user_id] = user.id
-            redirect_to user
+            session[:user_id] = @user.id
+            redirect_to @user
         else
             render :new
         end
